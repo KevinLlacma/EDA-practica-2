30a9076c4c09e11dc36fcc48763eebbce6519714
KevinLlacma <kllacma@unsa.edu.pe>
2023-06-17 11:54:19 -0500

    clase Elemento representa elemento en la cola de prioridad

--- PriorityQueueHeap.java
+++ PriorityQueueHeap.java
@@ -1,5 +1,6 @@
 public class PriorityQueueHeap<T extends Comparable<T>> {
-    
+    private Heap<Elemento<T>> heap;
+
     public PriorityQueueHeap(){
 
     }
@@ -17,6 +18,22 @@ public class PriorityQueueHeap<T extends Comparable<T>> {
 
     public T back() {
        
+    }
+    public static class Elemento<T> implements Comparable<Elemento<T>> {
+        private T x;
+        private int p;
+
+        public Elemento(T x, int p) {
+            this.x = x;
+            this.p = p;
+        }
+        public int compareTo(Elemento<T> other) {
+            return Integer.compare(p, other.getPrioridad());
+        }
+        public int getPrioridad() {
+            return p;
+        }
+
     }
 
    
